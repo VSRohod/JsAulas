be7a0dd72bdd400099b78050465943c7954e16f1
VSRohodTeacher <victorsantosrohod@gmail.com>
2023-12-14 07:51:05 -0300

    Update script.js

--- aula- API/materialDeAula/js/script.js
+++ aula- API/materialDeAula/js/script.js
@@ -1,12 +1,21 @@
+// API é a sigla em inglês para Application Programming Interface,
+//ouinterface de programação de aplicações. 
+//As interfaces de programação de aplicativos (APIs)
+//são conjuntos de ferramentas, 
+//definições e protocolos para a criação de aplicações de software. 
+//APIs conectam soluções e serviços, 
+//sem a necessidade de saber como esses elementos foram implementados.
+
 var consultarBtn = document.getElementById("consultar");
 
 consultarBtn.addEventListener("click", consulta)
 
 function consulta(){
     let cep = document.getElementById("cep").value;
-    if(cep.lenght > 8){
+    if(cep.length < 8){
         alert("Por favor insira as informações corretamente!")
     }else{
+        //fazer primeiro esse depois implementar if
         fetch(`https://viacep.com.br/ws/${cep}/json/`).then(i=>{
             return i.json()
         }).then(corpo => {
